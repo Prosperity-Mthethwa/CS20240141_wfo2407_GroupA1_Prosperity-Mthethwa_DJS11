e342dc115c397186fb18a87227eb0a09556d5d48
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-11-29 00:09:17 +0200

    components: ADDED CODE TO FOOTER AND HEADER...Added Routers functionality

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa'; // Import icons
+
+function Footer() {
+    const socialLinks = [
+        { name: 'Facebook', icon: FaFacebook, url: 'https://www.facebook.com/' },
+        { name: 'Twitter', icon: FaTwitter, url: 'https://twitter.com/' },
+        { name: 'Instagram', icon: FaInstagram, url: 'https://www.instagram.com/' },
+        { name: 'LinkedIn', icon: FaLinkedin, url: 'https://www.linkedin.com/' },
+    ];
+
+
+    return (
+        <footer className="footer bg-gray-800 text-white py-4">
+            <div className="container mx-auto flex justify-center items-center space-x-4">
+                {socialLinks.map((link) => (
+                    <a
+                        key={link.name}
+                        href={link.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-white hover:text-gray-300 transition-colors"
+                    >
+                        <link.icon size={24} />
+                    </a>
+                ))}
+            </div>
+            <div className="container mx-auto text-center mt-2">
+                <p>&copy; Mpilwenhle Prosperity Mthethwa. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
